84edcd65520e6c198e1a4ef1d61dfab036888e82
chuanhtoan <chuanhtoan139@gmail.com>
2020-06-28 17:30:14 +0700

    tao quyen

--- BaiLam/database/migrations/2020_06_14_134652_create_quyens_table.php
+++ BaiLam/database/migrations/2020_06_14_134652_create_quyens_table.php
@@ -16,6 +16,7 @@ class CreateQuyensTable extends Migration
         Schema::create('quyens', function (Blueprint $table) {
             $table->id();
             $table->string('tenQuyen',30);
+            $table->string('maQuyen',30);
             $table->string('moTa')->nullable();
             $table->timestamps();
         });
